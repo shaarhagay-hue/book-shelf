1b313f5486892fb80480a1c49495890b374afccf
shaarhagay-hue <shaarhagay@gmail.com>
2026-03-24 14:06:36 +0200

    Update books_app.py

--- books_app.py
+++ books_app.py
@@ -8,15 +8,8 @@ st.set_page_config(page_title="שבת אחת וסיימתם", page_icon="📚", 
 st.title("📚 שבת אחת וסיימתם")
 st.subheader("ניהול וחיפוש ספרים במקום אחד")
 
-# --- חלק 1: הוספת ספר (דרך טופס גוגל מוטמע) ---
-with st.expander("➕ הוספת ספר חדש לקטלוג", expanded=True):
-    # הקישור ששלחת מוטמע כאן:
-    google_form_url = "https://docs.google.com/forms/d/e/1FAIpQLSet8S6RvBmV-OLtFlRrBe3SBfpnwIkjekwOrYvlMyE2XER0Xw/viewform" 
-    
-    st.markdown(f'<iframe src="{google_form_url}" width="100%" height="600" frameborder="0" marginheight="0" marginwidth="0">טוען...</iframe>', unsafe_allow_html=True)
-    st.info("💡 לאחר לחיצה על 'שליחה' בטופס, רעננו את הדף (F5) כדי לראות את הספר בטבלה למטה.")
 
-# --- חלק 2: מנוע חיפוש ותצוגת הספרים ---
+# --- חלק 1: מנוע חיפוש ותצוגת הספרים ---
 st.write("---")
 st.write("### 🔍 חפשו ספר בקטלוג")
 
@@ -24,6 +17,14 @@ st.write("### 🔍 חפשו ספר בקטלוג")
 SHEET_ID = "1-mrlAW07r7OIDhrW7-abdeF4fkySZzkODD4bEg6T7kg"
 t = int(time.time()) # מונע מהדפדפן להציג נתונים ישנים מהזיכרון
 url = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv&cachebust={t}"
+
+# --- חלק 2: הוספת ספר (דרך טופס גוגל מוטמע) ---
+with st.expander("➕ הוספת ספר חדש לקטלוג", expanded=True):
+    # הקישור ששלחת מוטמע כאן:
+    google_form_url = "https://docs.google.com/forms/d/e/1FAIpQLSet8S6RvBmV-OLtFlRrBe3SBfpnwIkjekwOrYvlMyE2XER0Xw/viewform" 
+    
+    st.markdown(f'<iframe src="{google_form_url}" width="100%" height="600" frameborder="0" marginheight="0" marginwidth="0">טוען...</iframe>', unsafe_allow_html=True)
+    st.info("💡 לאחר לחיצה על 'שליחה' בטופס, רעננו את הדף (F5) כדי לראות את הספר בטבלה למטה.")
 
 try:
     # קריאת הנתונים וניקוי שורות ריקות
